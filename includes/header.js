413ef247f31b96f9f7a3ddb507de4684d31ab0aa
Martina Tan <36318786+martinatan@users.noreply.github.com>
2020-03-07 23:24:49 -0500

    Test dropdown menu

---
+++ includes/header.js
@@ -0,0 +1,19 @@
+document.write("<header>\
+	<div id=\"header-logo\">\
+		<a href=\"index.html\">The Reader's Nook</a>\
+	</div>\
+	<div class=\"topnav\">\
+			<a href=\"index.html\">Home</a>\
+            <div class=\"dropdown\">\
+            		<a href=\"about.html\" class=\"dropbtn\">About</a>\
+            	<div class=\"dropdown-content\">\
+    				<a href=\"mission.html\">Mission</a>\
+    				<a href=\"team.html\">Our Team</a>\
+    			</div>\
+            </div>\
+            <a href=\"bookcategories.html\">Books</a>\
+            <a href=\"getinvolved.html\">Get Involved</a>\
+            <a href=\"news.html\">In The News</a>\
+            <a href=\"contact.html\">Contact</a>\
+	</div>\
+</header>");
