586522cf4f50d6e661e417e38b46362a66d572ac
Martina Tan <36318786+martinatan@users.noreply.github.com>
2020-03-07 21:49:23 -0500

    Linked basic footer file

---
+++ footer.js
@@ -0,0 +1,12 @@
+document.write("<div class=\"footer\">\
+  	<p>© Ijams Educational Group 2020</p>\
+</div>");
+
+    // <div class=\"footer-left\">\
+    // <a href = \"https://www.facebook.com/RoomtoRead/\" target="_blank">\
+    // <img src=\"photos/footer/facebook.png\" style=\"width:42px; display:inline-block;\"></a>\
+    // <a href = \"https://twitter.com/RoomtoRead\" target=\"_blank\">\
+    // <img src=\"photos/footer/twitter.png\" style=\"width:42px;display:inline-block;\"></a>\
+    // <a href = \"https://www.instagram.com/roomtoread/\" target=\"_blank\">\
+    // <img src=\"photos/footer/instagram.png\" style=\"width:42px;display:inline-block;\"></a>\
+    // </div>\
