7b45b35a27f49105788a0f5b1cadab8169907926
Martina Tan <36318786+martinatan@users.noreply.github.com>
2020-03-08 12:05:53 -0400

    Clean up header

--- includes/header.js
+++ includes/header.js
@@ -5,14 +5,21 @@ document.write("<header>\
 	<div class=\"topnav\">\
 			<a href=\"index.html\">Home</a>\
             <div class=\"dropdown\">\
-            		<a href=\"about.html\" class=\"dropbtn\">About</a>\
+            	<a href=\"about.html\" class=\"dropbtn\">About</a>\
             	<div class=\"dropdown-content\">\
     				<a href=\"mission.html\">Mission</a>\
     				<a href=\"team.html\">Our Team</a>\
     			</div>\
             </div>\
             <a href=\"bookcategories.html\">Books</a>\
-            <a href=\"getinvolved.html\">Get Involved</a>\
+            <div class=\"dropdown\">\
+                <a href=\"getinvolved.html\" class=\"dropbtn\">Get Involved</a>\
+                <div class=\"dropdown-content\">\
+                    <a href=\"network.html\">Our Network</a>\
+                    <a href=\"volunteer.html\">Volunteer</a>\
+                    <a href=\"donate.html\">Donate</a>\
+                </div>\
+            </div>\
             <a href=\"news.html\">In The News</a>\
             <a href=\"contact.html\">Contact</a>\
 	</div>\
