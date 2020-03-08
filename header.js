7329b5929888f285f6f78c005e9575e1ec9c1507
Martina Tan <36318786+martinatan@users.noreply.github.com>
2020-03-07 21:24:17 -0500

    Linked basic header file and styles

---
+++ header.js
@@ -0,0 +1,15 @@
+document.write("<header>\
+	<div id=\"header-logo\">\
+		<a href=\"index.html\">The Reader's Nook</a>\
+	</div>\
+	<div id=\"topnav\">\
+		<ul>\
+			<li><a href=\"index.html\">Home</a></li>\
+            <li><a href=\"about.html\">About</a></li>\
+            <li><a href=\"bookcategories.html\">Books</a></li>\
+            <li><a href=\"getinvolved.html\">Get Involved</a></li>\
+            <li><a href=\"news.html\">In The News</a></li>\
+            <li><a href=\"contact.html\">Contact</a></li>\
+		</ul>\
+	</div>\
+</header>");
