56d65aa1e07798cd47754681589f36b56dbfe071
Martina Tan <36318786+martinatan@users.noreply.github.com>
2020-03-08 23:03:21 -0400

    Adjust footer

--- includes/footer.js
+++ includes/footer.js
@@ -1,10 +1,10 @@
-document.write('<footer style="display:inline-block;">\
-    <div class=\"social\">\
-        <a href=\"https://www.facebook.com/RoomtoRead/\" target=\"_blank\"><img style=\"width:42px; display:inline-block;\" src=\"photos/footer/facebook.png\"></a>\
-        <a href=\"https://twitter.com/RoomtoRead\" target=\"_blank\"><img style=\"width:42px; display:inline-block;\" src=\"photos/footer/twitter.png\"></a>\
-        <a href=\"https://www.instagram.com/roomtoread/\" target=\"_blank\"><img style=\"width:42px; display:inline-block;\" src=\"photos/footer/instagram.png\"></a>\
+document.write('<footer>\
+    <div id=\"social\">\
+        <a href=\"https://www.facebook.com/RoomtoRead/\" target=\"_blank\"><img style=\"width:36px;\" src=\"photos/footer/facebook.png\"></a>\
+        <a href=\"https://twitter.com/RoomtoRead\" target=\"_blank\"><img style=\"width:36px;\" src=\"photos/footer/twitter.png\"></a>\
+        <a href=\"https://www.instagram.com/roomtoread/\" target=\"_blank\"><img style=\"width:36px;\" src=\"photos/footer/instagram.png\"></a>\
     </div>\
-    <div class=\"copyright\">\
-        <p style=\"display:inline-block; float:right;\" align=\"right\">© IJAMS Educational Group 2020</p>\
+    <div id=\"copyright\">\
+        <p>&copy; IJAMS Educational Group 2020</p>\
     </div>\
 </footer>');
